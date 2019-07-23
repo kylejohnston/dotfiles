eab88b4f9f76ebfd7a25915e6e8b861b70195718
Kyle Johnston <kyle.johnston@stayalfred.com>
2019-07-23 09:43:33 -0700

    Added NPM aliases for run/watch

--- aliases.zsh
+++ aliases.zsh
@@ -37,3 +37,7 @@ alias weather="curl -4 http://wttr.in"
 alias resize="sips --resampleWidth 600 formatOptions 100 *"
 alias resize14="sips --resampleWidth 1440 formatOptions 100 *"
 alias convert="sips -s format jpeg ./*.png --out ."
+
+# NPM
+alias npmw="npm run watch"
+alias npmd="npm run dev"
