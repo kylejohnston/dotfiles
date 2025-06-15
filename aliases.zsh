30a83132f60ae4c86372be6cbc7bb9676bb36b58
Kyle Johnston <flow14@gmail.com>
2025-06-15 14:44:59 -0500

    updated you-get, nvim

--- aliases.zsh
+++ aliases.zsh
@@ -11,8 +11,10 @@ alias rz="source ~/.zshrc"
 
 alias gs="git status"
 alias o.="open ."
-alias yg="you-get"
+alias yg="yt-dlp"
+# 2025-02-15 swapped you-get for yt-dlp
 alias sn="sudo nano"
+alias vim="nvim"
 
 alias weather="curl -4 http://wttr.in"
 
@@ -21,3 +23,12 @@ alias sassw="sass --watch scss:css"
 
 # Local Dev / Laravel
 alias sql="mysql.server start"
+
+alias code='open -b com.microsoft.VSCode'
+
+# alias python=/usr/local/bin/python3
+# alias pip=/usr/local/pip3
+
+:take () {
+    mkdir -p $@ && cd ${@:$#}
+}
